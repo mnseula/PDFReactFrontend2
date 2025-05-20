85a57fb8124bd6b3750d83cbaa41a1f034282d7d
mnseula <mcnseula@gmail.com>
2025-05-20 08:16:26 -0400

    Create processing_options.dart

---
+++ lib/models/processing_options.dart
@@ -0,0 +1,34 @@
+lib/models/processing_options.dart
+import 'dart:typed_data';
+
+class ProcessingOptions {
+  final String operation;
+  final Map<String, dynamic> parameters;
+  
+  ProcessingOptions({required this.operation, required this.parameters});
+}
+
+class RectangleArea {
+  final double x1, y1, x2, y2;
+  final int? pageNumber;
+  
+  RectangleArea({
+    required this.x1,
+    required this.y1,
+    required this.x2, 
+    required this.y2,
+    this.pageNumber
+  });
+}
+
+class AnnotationOptions {
+  final String type;
+  final RectangleArea area;
+  final String? signatureImage; // Base64
+  
+  AnnotationOptions({
+    required this.type,
+    required this.area,
+    this.signatureImage
+  });
+}
