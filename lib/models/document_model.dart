592184b25574c40b43d61a535d49fd5b2c036188
mnseula <mcnseula@gmail.com>
2025-05-19 22:15:35 -0400

    Create document_model.dart

---
+++ lib/models/document_model.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/foundation.dart';
+
+class Document {
+  final String id;
+  final String name;
+  final String path;
+  final int size;
+  final DateTime uploadDate;
+  final String? thumbnailUrl;
+  final DocumentType type;
+
+  Document({
+    required this.id,
+    required this.name,
+    required this.path,
+    required this.size,
+    required this.uploadDate,
+    this.thumbnailUrl,
+    required this.type,
+  });
+}
+
+enum DocumentType {
+  pdf,
+  word,
+  powerpoint,
+  excel,
+  image,
+  other,
+}
